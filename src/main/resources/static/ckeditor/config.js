428e047a0c3837c94358cde30c36bb55cf8e0323
pinwormmy <ozkhan@naver.com>
2025-07-28 01:39:23 +0900

    CK에디터에서 html소스편집 기능 수정
    소스편집 모드에서 글자색 안 보이던 것 수정
    유튜브 링크 걸 수 있게 아이프레임 허용
    
    호스팅 업데이트 완료

--- src/main/resources/static/ckeditor/config.js
+++ src/main/resources/static/ckeditor/config.js
@@ -1,10 +1,11 @@
 CKEDITOR.editorConfig = function( config ) {
   // 기본 설정 예시
-  config.language = 'ko'; // 한국어 설정
-  config.uiColor = '#f4f4f4'; // 에디터 UI 색상 변경
-  config.height = 600; // 에디터 높이 조절
+  config.language = 'ko';
+  config.uiColor = '#000112'; // 에디터 UI 색상 변경
+  config.height = 500; // 에디터 높이 조절
   config.versionCheck = false; // 버전 경고 없애기
   config.clipboard_handleImages = false; // 이미지 업로드 코드는 따로 있어서 여기선 비활성화
+  config.extraAllowedContent = 'iframe[*]';
   config.toolbar = [
     { name: 'document', items: [ 'Source', '-', 'Save', 'NewPage', 'Preview', 'Print', '-', 'Templates' ] },
     { name: 'clipboard', items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
