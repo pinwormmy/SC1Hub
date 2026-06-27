a72d2f08360c0ad81e3dbc41550a749f1510fbbb
studio midori <ozkhan@naver.com>
2026-06-27 09:51:04 +0900

    배포 재시작 순서 안정화

--- src/main/java/com/sc1hub/SC1HubApplication.java
+++ src/main/java/com/sc1hub/SC1HubApplication.java
@@ -4,8 +4,6 @@ import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @MapperScan(basePackages = {"com.sc1hub.board.mapper", "com.sc1hub.member.mapper", "com.sc1hub.visitor.mapper",
@@ -13,15 +11,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
         annotationClass = Mapper.class)
 @SpringBootApplication
 @EnableScheduling
-public class SC1HubApplication extends SpringBootServletInitializer {
+public class SC1HubApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SC1HubApplication.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
-        return applicationBuilder.sources(ServletInitializer.class);
     }
 
 }
